5f55dbb3a09e35df623aa3e540c1a128e28b9698
Strazil001 <strazil001@gmail.com>
2021-07-25 20:14:32 +0200

    intro

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 function hamburgerToggle(){
 	var x = document.getElementById("links");
+	var y = document.querySelector(".icon");
+	var c = document.querySelector("#icon-close");
 	if(x.style.display === "inline-block"){
 		x.style.display = "none";
+		y.style.color = "antiquewhite";
+		y.style.display = "inline-block";
+		c.style.display = "none";
 	}
 	else{
 		x.style.display = "inline-block";
+		y.style.display = "none";
+		c.style.display = "inline-block";
+		c.style.color = "grey";
 	}
 }
